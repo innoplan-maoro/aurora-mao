66ec02dab1d00fc5c4e0e2d00b2db1c463fd33d0
Andre Maoro <andre.maoro@inno-plan.de>
2026-01-20 11:08:56 +0100

    add teamviewer signature, add krdc, remove unneeded packages, add edge policies

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,10 +9,21 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# teamviewer
+wget -O /tmp/teamviewer.asc https://linux.teamviewer.com/pubkey/currentkey.asc
+rpm --import /tmp/teamviewer.asc
+rm -f /tmp/teamviewer.asc
 dnf5 install -y https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm
 
-# this installs a package from fedora repos
-#dnf5 install -y konsole
+# install packages from fedora repos
+dnf5 install -y \
+    krdc \
+
+# remove pre-installed packages
+dnf5 remove -y \
+    openrazer-daemon \
+    openrgb-udev-rules \
+    tailscale
 
 # Use a COPR Example:
 #
@@ -24,3 +35,37 @@ dnf5 install -y https://download.teamviewer.com/download/linux/teamviewer.x86_64
 #### Example for enabling a System Unit File
 
 #systemctl enable podman.socket
+
+# edge policies
+cat <<EOF > /etc/opt/edge/policies/managed/policies.json
+{
+  "HideFirstRunExperience": true,
+  "ShowPDFDefaultRecommendationsEnabled": false,
+  "SpotlightExperiencesAndRecommendationsEnabled": false,
+  "NewTabPageSearchBox": "redirect",
+  "GenAILocalFoundationalModelSettings": true,
+  "WebToBrowserSignInEnabled": false,
+  "StartupBoostEnabled": false,
+  "NewTabPageBingChatEnabled": false,
+  "NewTabPageContentEnabled": false,
+  "NewTabPageHideDefaultTopSites": true,
+  "AIGenThemesEnabled": false,
+  "AutoImportAtFirstRun": 4,
+  "BuiltInAIAPIsEnabled": false,
+  "BuiltInDnsClientEnabled": false,
+  "ComposeInlineEnabled": false,
+  "CopilotPageContext": false,
+  "DefaultBrowserSettingEnabled": false,
+  "DefaultBrowserSettingsCampaignEnabled": false,
+  "DiagnosticData": false,
+  "EdgeShoppingAssistantEnabled": false,
+  "Microsoft365CopilotChatIconEnabled": false,
+  "ShowAcrobatSubscriptionButton": false,
+  "ShowMicrosoftRewards": false,
+  "ShowRecommendationsEnabled": false,
+  "TabServicesEnabled": false,
+  "TextPredictionEnabled": false,
+  "VisualSearchEnabled": false,
+  "EdgeHistoryAISearchEnabled": false
+}
+EOF
